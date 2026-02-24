8a703ae0249daa7894cae3e30e1e3b6f6dd7a349
Nagendra Kumar Yanamadala <NYanamadala+sscinc@sscinc.com>
2026-02-25 01:13:46 +0530

    update appspec file for windows

--- aws/scripts/copy-appspec.sh
+++ aws/scripts/copy-appspec.sh
@@ -2,12 +2,13 @@ echo "TargetOS='$AWD_TARGET'"
 
 cp ./aws/linux_appspec.yml appspec.yml
 
-if [ "$AWD_TARGET" = "LinJboss" ||  "$AWD_TARGET" = "LinuxLiberty"  ]
+if [ "$AWD_TARGET" = "LinJboss" ] || [ "$AWD_TARGET" = "LinuxLiberty" ]
 then
    cp ./aws/linux_appspec.yml appspec.yml
-elif [ "$AWD_TARGET" = "WindowsLiberty" ||  "$AWD_TARGET" = "WindowsJboss" ]
+elif [ "$AWD_TARGET" = "WindowsLiberty" ] || [ "$AWD_TARGET" = "WindowsJboss" ]
 then
    cp ./aws/windows_appspec.yml appspec.yml
-else 
-   echo 'Invalid TargetOS' 
+else
+   echo "Invalid TargetOS"
+   exit 1
 fi
