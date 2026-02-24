121b61de0253e90ca0029f6f9a2703b44ec23882
Nagendra Kumar Yanamadala <NYanamadala+sscinc@sscinc.com>
2026-02-24 22:00:38 +0530

    run sh file changed

--- cmd/run.sh
+++ cmd/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/lib/jvm/java-21/bin/java -jar "/opt/dst/schema-services/schemaservice/schema-service-1.0.jar"
+/usr/lib/jvm/java/bin/java -jar "/opt/dst/schema-services/schemaservice/schema-service-1.0.jar"
